f41117a8f528d19b862037b1619c341d01b33b93
Kunal Nain <nainkunal933@gmail.com>
2019-11-13 13:06:09 -0800

    Removed PyGithub and started using REST V3 API calls

--- data/data_parser.py
+++ data/data_parser.py
@@ -1,32 +1,34 @@
 import json
 import os
-from github import Github
 from dotenv import load_dotenv, find_dotenv
 from datetime import datetime
 import requests
 
 load_dotenv(find_dotenv())
 
-# json_file = open("./raw_data.json", "r")
-# raw_data = json.load(json_file)
-# print(json.dumps(raw_data, indent=4, sort_keys=True))
+owner = "sequelize"
+repos = "sequelize"
+response = requests.get("https://api.github.com/repos/%s/%s/pulls?state=closed&page=1&per_page=1" % (owner, repos), headers={'Authorization': 'token %s' % (os.getenv("access_token"))})
+json_response = response.json()
 
+for pr in json_response:
+    status_check = requests.get("https://api.github.com/repos/%s/%s/commits/%s/status" % (owner, repos, pr["head"]["sha"]), headers={'Authorization': 'token %s' % (os.getenv("access_token"))})
+    status_check = status_check.json()
 
+    user_contr = requests.get("https://api.github.com/repos/%s/%s/commits?author=%s&per_page=100" % (owner, repos, pr["head"]["user"]["login"]), headers={'Authorization': 'token %s' % (os.getenv("access_token"))})
 
-g = Github(os.getenv("access_token"), per_page=30)
+    comment_count = requests.get("https://api.github.com/repos/%s/%s/issues/%s/comments?per_page=100" % (owner, repos, pr["number"]), headers={'Authorization': 'token %s' % (os.getenv("access_token"))})
 
-repo = g.get_repo("Sequelize/Sequelize")
-print(repo)
-pulls = repo.get_pulls(state='closed').get_page(0)
-for pr in pulls:
-    print(pr.user)
-    print("PR Number = ", pr.number)
-    # response = requests.get(pr.html_url)
-    # print(response)
-    # data = response.json()
-    # print(data)
-    # print("PR User Type = ", data.author_association)
-    print("PR User Contribution History = ", repo.get_commits(author=pr.user).totalCount)
-    print("PR Conservation History (Comment Count) = ", pr.get_issue_comments().totalCount)
-    print("PR Time Duration = ", pr.closed_at - pr.created_at)
-    print("PR Is Merged = ", pr.is_merged(), "\n")
+    closed_at = pr["closed_at"]
+    created_at = pr["created_at"]
+    created_at_timeobject = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ")
+    closed_at_timeobject = datetime.strptime(closed_at, "%Y-%m-%dT%H:%M:%SZ")
+    time = closed_at_timeobject - created_at_timeobject
+
+    print("PR Number = ", pr["number"])
+    print("PR Status Check = ", status_check["state"])
+    print("PR User Type = ", pr["author_association"])
+    print("PR User Contribution History = ", len(user_contr.json()))
+    print("PR Conversation History (Comment Count) = ", len(comment_count.json()))
+    print("PR Time Duration", time.total_seconds())
+    print("PR Is Merged = ", pr["merged_at"], "\n")
