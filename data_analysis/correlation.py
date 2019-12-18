42f6bd3a58cc35ac50e2351f3f10a90e166bff0f
Kunal Nain <nainkunal933@gmail.com>
2019-12-17 18:16:10 -0800

    Initial Commit

--- data_analysis/correlation.py
+++ data_analysis/correlation.py
@@ -1,16 +1,21 @@
+# Author: Kunal Nain
+
 import numpy as np
 import json
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 import os
 
+# Opening the norm_output.json file which contains the normalized data
 os.chdir('..')
 filepath = os.getcwd()
 filepath = filepath + "/data/norm_output.json"
 f = open(filepath, "r")
 
+# Loading the files data as JSON
 json_data = json.loads(f.read())
 
+# Creating lists to hold the data
 x_status_check = []
 x_user_type = []
 x_user_contr = []
@@ -18,6 +23,7 @@ x_comment_count = []
 x_time_duration = []
 y = []
 
+# Parsing the data and loading the lists
 for data_object in json_data:
     x_status_check.append(data_object['pr_status_check'])
     x_user_type.append(data_object['pr_user_type'])
@@ -26,6 +32,7 @@ for data_object in json_data:
     x_time_duration.append(data_object['pr_time_duration'])
     y.append(data_object['pr_is_merged'])
 
+# Converting the lists into Numpy Array
 x_array_status_check = np.array(x_status_check)
 x_array_user_type = np.array(x_user_type)
 x_array_user_contr = np.array(x_user_contr)
@@ -34,6 +41,7 @@ x_array_time_duration = np.array(x_time_duration)
 
 y_array = np.array(y)
 
+# Reshaping the array so that it is compatible with the Linear Regression method
 x_array_status_check = x_array_status_check.reshape(-1, 1)
 x_array_user_type = x_array_user_type.reshape(-1, 1)
 x_array_user_contr = x_array_user_contr.reshape(-1, 1)
@@ -42,6 +50,7 @@ x_array_time_duration = x_array_time_duration.reshape(-1, 1)
 
 y_array = y_array.reshape(-1, 1)
 
+# Calculating the R-Squared
 reg_status_check = LinearRegression().fit(x_array_status_check, y_array)
 reg_user_type = LinearRegression().fit(x_array_user_type, y_array)
 reg_user_contr = LinearRegression().fit(x_array_user_contr, y_array)
